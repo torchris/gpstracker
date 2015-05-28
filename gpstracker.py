408b406bc75da45863bd62834a0842ab362e43df
torchris <torchris4@gmail.com>
2015-05-28 01:44:13 +0000

    First commit

---
+++ gpstracker.py
@@ -0,0 +1,82 @@
+#! /usr/bin/python
+# Written by Dan Mandle http://dan.mandle.me September 2012
+# License: GPL 2.0
+ 
+import os
+from gps import *
+from time import *
+import time
+import threading
+
+from oled.device import ssd1306, sh1106
+from oled.render import canvas
+from PIL import ImageFont
+
+font = ImageFont.load_default()
+device = sh1106(port=1, address=0x3C)
+ 
+gpsd = None #seting the global variable
+ 
+os.system('clear') #clear the terminal (optional)
+ 
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+ 
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+ 
+if __name__ == '__main__':
+  gpsp = GpsPoller() # create the thread
+  try:
+    gpsp.start() # start it up
+    while True:
+      #It may take a second or two to get good data
+      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+ 
+      os.system('clear')
+ 
+      print
+      print ' GPS reading'
+      print '----------------------------------------'
+      print 'latitude    ' , gpsd.fix.latitude
+      print 'longitude   ' , gpsd.fix.longitude
+      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      print 'altitude (m)' , gpsd.fix.altitude
+      print 'eps         ' , gpsd.fix.eps
+      print 'epx         ' , gpsd.fix.epx
+      print 'epv         ' , gpsd.fix.epv
+      print 'ept         ' , gpsd.fix.ept
+      print 'speed (m/s) ' , gpsd.fix.speed
+      print 'climb       ' , gpsd.fix.climb
+      print 'track       ' , gpsd.fix.track
+      print 'mode        ' , gpsd.fix.mode
+      print
+      print 'sats        ' , gpsd.satellites
+      lati = str(gpsd.fix.latitude)
+      longi = str(gpsd.fix.longitude)
+      alti = str(gpsd.fix.altitude)
+
+      with canvas(device) as draw:
+       font = ImageFont.load_default()
+       draw.text((5, 5), "Lat: ", font=font, fill=255)       
+       draw.text((35,5), lati, font=font, fill=255)
+       draw.text((5, 20), "Long: ", font=font, fill=255)       
+       draw.text((35,20), longi, font=font, fill=255)
+       draw.text((5, 35), "Alt: ", font=font, fill=255)       
+       draw.text((35,35), alti, font=font, fill=255)
+ 
+      time.sleep(5) #set to whatever
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+  print "Done.\nExiting."
+
