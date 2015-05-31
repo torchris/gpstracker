008a2ab196757403e14fccbae83e22ae6248c610
torchris <torchris4@gmail.com>
2015-05-31 00:32:08 +0000

    Update

--- gpstracker.py
+++ gpstracker.py
@@ -7,6 +7,7 @@ from gps import *
 from time import *
 import time
 import threading
+import math
 
 from oled.device import ssd1306, sh1106
 from oled.render import canvas
@@ -16,6 +17,9 @@ font = ImageFont.load_default()
 device = sh1106(port=1, address=0x3C)
  
 gpsd = None #seting the global variable
+cummDist = 0
+old_lati = 0
+old_longi = 0
  
 os.system('clear') #clear the terminal (optional)
  
@@ -31,22 +35,45 @@ class GpsPoller(threading.Thread):
     global gpsd
     while gpsp.running:
       gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+
+def getDistance(lat1,lon1,lat2,lon2):
+    # This uses the haversine formula, which remains a good numberical computation,
+    # even at small distances, unlike the Shperical Law of Cosines.
+    # This method has ~0.3% error built in.
+    R = 6371 # Radius of Earth in km
+
+    dLat = math.radians(float(lat2) - float(lat1))
+    dLon = math.radians(float(lon2) - float(lon1))
+    lat1 = math.radians(float(lat1))
+    lat2 = math.radians(float(lat2))
+
+    a = math.sin(dLat/2) * math.sin(dLat/2) + \
+        math.cos(lat1) * math.cos(lat2) * math.sin(dLon/2) * math.sin(dLon/2)
+
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    d = (R * c) * 1000 #output in meters
+
+    return d
  
 if __name__ == '__main__':
   gpsp = GpsPoller() # create the thread
   try:
     gpsp.start() # start it up
     while True:
+      global cummDist
+      global old_lati
+      global old_longi 
       #It may take a second or two to get good data
       #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
- 
+
       os.system('clear')
- 
+
       print
       print ' GPS reading'
       print '----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
+      print 'latitude    ' , round(gpsd.fix.latitude,5)
+      print 'longitude   ' , round(gpsd.fix.longitude,5)
       print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
       print 'altitude (m)' , gpsd.fix.altitude
       print 'eps         ' , gpsd.fix.eps
@@ -59,19 +86,33 @@ if __name__ == '__main__':
       print 'mode        ' , gpsd.fix.mode
       print
       print 'sats        ' , gpsd.satellites
-      lati = str(gpsd.fix.latitude)
-      longi = str(gpsd.fix.longitude)
+      
+
+      lati = str(round(gpsd.fix.latitude,5))
+      longi = str(round(gpsd.fix.longitude,5))
       alti = str(gpsd.fix.altitude)
+      sped = str(gpsd.fix.speed)
+      print "Old Latitude:  ", old_lati
+      print "Old Longitude:  ", old_longi
+      cummDistNum = round(getDistance(old_lati,old_longi,gpsd.fix.latitude,gpsd.fix.longitude),2)
+      cummDist = str(cummDistNum)
+      print "Distance between samples: ", cummDist
 
       with canvas(device) as draw:
        font = ImageFont.load_default()
-       draw.text((5, 5), "Lat: ", font=font, fill=255)       
-       draw.text((35,5), lati, font=font, fill=255)
-       draw.text((5, 20), "Long: ", font=font, fill=255)       
-       draw.text((35,20), longi, font=font, fill=255)
-       draw.text((5, 35), "Alt: ", font=font, fill=255)       
-       draw.text((35,35), alti, font=font, fill=255)
- 
+       draw.text((5, 3), "Lat: ", font=font, fill=255)       
+       draw.text((35,3), lati, font=font, fill=255)
+       draw.text((5, 15), "Long: ", font=font, fill=255)       
+       draw.text((35,15), longi, font=font, fill=255)
+       draw.text((5, 27), "Alt: ", font=font, fill=255)       
+       draw.text((35,27), alti, font=font, fill=255)
+       draw.text((5, 40), "Spd: ", font=font, fill=255)       
+       draw.text((35,40), sped, font=font, fill=255) 
+       draw.text((5, 52), "Dst: ", font=font, fill=255)       
+       draw.text((35,52), cummDist, font=font, fill=255) 
+      
+      old_lati = round(gpsd.fix.latitude,5)
+      old_longi = round(gpsd.fix.longitude,5)
       time.sleep(5) #set to whatever
  
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
